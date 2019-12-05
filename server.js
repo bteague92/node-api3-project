1e40f612d5139bc17025c84c3d911198dd535173
[Brandon Teague] <[teaguebrandon@outlook.com]>
2019-12-04 19:53:11 -0500

    set up add user, remove user, get all users, and get user by id

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 
+const userRouter = require("./users/userRouter.js")
+
 const server = express();
+
+server.use(express.json());
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -12,5 +16,7 @@ function logger(req, res, next) {
   console.log(`${req.method} to ${req.originalUrl}`);
   next();
 }
+
+server.use('/api/users', userRouter);
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,11 +1,20 @@
 const express = require('express');
 
-const router = express.Router();
+const router = express.Router(); /// 
 
 const users = require("./userDb.js")
 
 router.post('/', (req, res) => {
-  // do your magic!
+  users.insert(req.body)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: 'We f***** up',
+      });
+    });
 });
 
 router.post('/:id/posts', (req, res) => {
@@ -13,29 +22,96 @@ router.post('/:id/posts', (req, res) => {
 });
 
 router.get('/', (req, res) => {
-  // do your magic!
+  users.get()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: 'We f***** up',
+      });
+    });
 });
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+  const id = req.params.id;
+  users.getById(id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({ message: "Couldnt find that dude" })
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: "We f***** up",
+      });
+    });
 });
 
 router.get('/:id/posts', (req, res) => {
-  // do your magic!
+  users.getUserPosts(req.params.id)
+    .then(posts => {
+      if (posts.length > 0) {
+        res.status(200).json(posts);
+      } else {
+        res.status(404).json({ message: "That guy didnt write any posts" })
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: "We f***** up"
+      });
+    });
 });
 
 router.delete('/:id', (req, res) => {
-  // do your magic!
+  const id = req.params.id;
+  users.remove(id)
+    .then(user => {
+      if (user) {
+        res.status(200).json({ message: 'Theyre swimming with the fishes' });
+      } else {
+        res.status(404).json({ message: 'Couldnt find that dude' });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: 'We f***** up',
+      });
+    });
 });
 
 router.put('/:id', (req, res) => {
-  // do your magic!
+  const id = req.params.id;
+  const changes = req.body;
+  users.update(id, changes)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user)
+      } else {
+        res.status(404).json({
+          message: "That dude doesnt exist"
+        })
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: "We f***** up"
+      })
+    })
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+
 }
 
 function validateUser(req, res, next) {
